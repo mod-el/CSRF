ef8554153079edea2a49ba4ea6faedcfef2b36ea
Domenico Giambra <sviluppo@editt.it>
2018-01-16 21:43:43 +0100

    First versions of manifest and class

---
+++ CSRF.php
@@ -0,0 +1,36 @@
+<?php namespace Model\CSRF;
+
+use Model\Core\Module;
+
+class CSRF extends Module
+{
+	/**
+	 * @param array $options
+	 */
+	public function init(array $options)
+	{
+		if (!isset($_SESSION['csrf']))
+			$_SESSION['csrf'] = md5(uniqid(rand(), true));
+	}
+
+	/**
+	 * Checks if the token was sent and it's correct
+	 *
+	 * @return bool
+	 */
+	public function checkCsrf()
+	{
+		if (isset($_POST['c_id']) and $_POST['c_id'] === $_SESSION['csrf'])
+			return true;
+		else
+			return false;
+	}
+
+	/**
+	 * Print a hidden input with the token as value
+	 */
+	public function csrfInput()
+	{
+		echo '<input type="hidden" name="c_id" value="' . entities($_SESSION['csrf']) . '" />';
+	}
+}
